ad44bd8625f8e142d4d1dae2acfd05c4766e2879
Sydney Wong <syd.wong888@gmail.com>
2022-04-27 13:49:15 -0700

    add failing test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,35 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.ArrayList;
 public class MarkdownParseTest {
 
     @Test
     public void addition() {
-        assertEquals(2, 1 + 1);
+        assertEquals(2, 1 + 0);
     }
 
+    @Test
+    public void testFile1() throws IOException {
+        Path filePath = Path.of("./test-file.md");
+        String content = Files.readString(filePath);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        List<String> correctLinks = List.of("https://something.com", "some-thing.html");
+        assertEquals(correctLinks, links);
+    }
+
+
 }
+
+// import static org.junit.Assert.*;
+// import org.junit.*;
+// public class MarkdownParseTest {
+//     @Test
+//     public void addition() {
+//         assertEquals(2, 1 + 1);
+//     }
+// }
